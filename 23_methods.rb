d93acd7a83726f571bc6ce324931c4eb3e5356a6
Cynthia Salazar <cinthya3259@gmail.com>
2021-01-29 14:45:47 -0600

    Metodos

---
+++ 23_methods.rb
@@ -0,0 +1,15 @@
+def twice (thing)
+thing 
+end
+
+test_variable = twice (4)
+puts
+print test_variable
+
+test_variable = twice ([2,4,6])
+puts
+print test_variable
+
+test_variable = twice("Hola")
+puts
+print test_variable
